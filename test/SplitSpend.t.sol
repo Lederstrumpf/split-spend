848e6b619d786c1b74f8389120c358e59ed7b558
Robert Hambrock <roberthambrock@gmail.com>
2024-06-07 13:30:36 +0200

    refactor & improve tests

--- test/SplitSpend.t.sol
+++ test/SplitSpend.t.sol
@@ -15,16 +15,17 @@ contract SplitSpendTest is Test {
     function setUp() public {
         // Deploy the SplitSpend contract
         splitSpend = new SplitSpend(alice, bob, predefinedDestination);
-    }
 
-    function testAliceSpendToPredefinedDestination() public {
-        // Set Alice as the caller
-        vm.prank(alice);
         // Send some ether to the contract
         (bool sent, ) = address(splitSpend).call{value: 1 ether}("");
         assertTrue(sent);
+    }
 
+    function testAliceSpendToPredefinedDestination() public {
         uint256 predefinedDestinationStartBalance = predefinedDestination.balance;
+
+        // Set Alice as the caller
+        vm.prank(alice);
 
         // Call spendToPredefinedDestination as Alice
         splitSpend.spendToPredefinedDestination();
@@ -34,40 +35,45 @@ contract SplitSpendTest is Test {
     }
 
     function testBobSpendFreely() public {
+        // Set the destination address
+        address payable arbitraryAddress = payable(address(0x4));
+
+        uint256 arbitraryAddressStartBalance = arbitraryAddress.balance;
+
         // Set Bob as the caller
         vm.prank(bob);
-        // Send some ether to the contract
-        (bool sent, ) = address(splitSpend).call{value: 1 ether}("");
-        assertTrue(sent);
-
-        // Set the destination address
-        address payable randomAddress = payable(address(0x4));
-
-        uint256 randomAddressStartBalance = randomAddress.balance;
 
         // Call spendFreely as Bob to the random address
-        splitSpend.spendFreely(randomAddress);
+        splitSpend.spendFreely(arbitraryAddress);
 
         // Assert that the random address received the 1 ether
-        assertEq(randomAddress.balance, randomAddressStartBalance + 1 ether);
+        assertEq(arbitraryAddress.balance, arbitraryAddressStartBalance + 1 ether);
     }
 
     function testFailOnlyAliceCanCallSpendToPredefinedDestination() public {
+        uint256 predefinedDestinationStartBalance = predefinedDestination.balance;
+
         // Set Bob as the caller
         vm.prank(bob);
 
         // Call spendToPredefinedDestination as Bob
         splitSpend.spendToPredefinedDestination();
+
+        assertEq(predefinedDestination.balance, predefinedDestinationStartBalance);
     }
 
     function testFailOnlyBobCanCallSpendFreely() public {
+        // Set the destination address
+        address payable arbitraryAddress = payable(address(0x4));
+
+        uint256 arbitraryAddressStartBalance = arbitraryAddress.balance;
+
         // Set Alice as the caller
         vm.prank(alice);
 
-        // Set the destination address
-        address payable randomAddress = payable(address(0x4));
-
         // Call spendFreely as Alice
-        splitSpend.spendFreely(randomAddress);
+        splitSpend.spendFreely(arbitraryAddress);
+
+        assertEq(arbitraryAddressStartBalance, arbitraryAddress.balance);
     }
 }
